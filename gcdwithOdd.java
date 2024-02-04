cb9705f645e9c7cfccde482bf6394c62297e9008
Rishi Bose <69758866+RishiBose04@users.noreply.github.com>
2024-02-04 11:04:02 +0530

    Add files via upload

---
+++ gcdwithOdd.java
@@ -0,0 +1,45 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class HelloWorld {
+    // Maximum
+    static int Max(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < 5; i++) {
+            if (max <= array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    // Minimum
+    static int Min(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < 5; i++) {
+            if (min >= array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    static void LCM(int x, int y) {
+        int gcd = 1;
+        for (int i = 1; i <= x && i <= y; ++i) {
+            if (x % i == 0 && y % i == 0) {
+                gcd = i;
+            }
+        }
+        System.out.println(gcd);
+        int lcm = (x * y) / gcd;
+        System.out.println(lcm);
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 3, 6, 4, 5 };
+        int mx = Max(arr);
+        int mn = Min(arr);
+        LCM(mx, mn);
+    }
+}
